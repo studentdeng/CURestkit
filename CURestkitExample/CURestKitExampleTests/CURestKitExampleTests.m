57c3a41e5f5c5266f49f36aeaaaeb7911dee0e1e
studentdeng <studentdeng@hotmail.com>
2013-09-11 17:12:23 +0800

    remove

---
+++ CURestkitExample/CURestKitExampleTests/CURestKitExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  CURestKitExampleTests.m
+//  CURestKitExampleTests
+//
+//  Created by curer on 9/11/13.
+//  Copyright (c) 2013 curer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CURestKitExampleTests : XCTestCase
+
+@end
+
+@implementation CURestKitExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
